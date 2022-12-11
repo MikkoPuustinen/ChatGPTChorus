f532cf1241e6845e900053e70bb17541ed0bfaf7
MikkoPuustinen <mikkowr@gmail.com>
2022-12-11 23:26:36 +0200

    Use ai to clean up the code and improve documentation

--- ChatGPTChorus.cpp
+++ ChatGPTChorus.cpp
@@ -12,14 +12,9 @@
 
 // Define the maximum delay length and default delay value
 constexpr int kMaxDelay = 1200;
-constexpr int kDefaultDelay = 0;
 
-// Define the LFO frequency and depth
-constexpr float kLfoFrequency = 0.1;
-constexpr float kLfoDepth = 10.0;
-
-const float min_delay = 0.01;  // Minimum delay = 10 ms
-const float max_delay = 0.025;  // Maximum delay = 25 ms
+const float min_delay = 0.01; // Minimum delay = 10 ms
+const float max_delay = 0.025; // Maximum delay = 25 ms
 
 // Define a circular buffer type for storing delay line samples
 using DelayLine = std::array<float, kMaxDelay>;
@@ -30,9 +25,8 @@ DelayLine delay_line{ };
 // Implement a function that applies a variable length delay to an input sample
 float Delay(float input, float delay, int sampleRate) {
 	// Add the input sample to the delay line
-
-
 	delay_line[0] = input;
+
 	// Calculate the position of the delayed sample in the delay line
 	float delay_samples = delay * sampleRate;
 
@@ -53,21 +47,22 @@ float Delay(float input, float delay, int sampleRate) {
 	return output;
 }
 
+// Implement a function that generates a modulated low-frequency oscillator (LFO) value
 float ModulatedLfo(int time, float frequency, float depth) {
-	// Compute the LFO value
+	// Compute the LFO value using the sine function
 	float lfo = sin(2 * M_PI * frequency * time);
 
-	// Modulate the LFO value
+	// Modulate the LFO value by applying the sine function again
 	float modulated = depth * sin(lfo);
 
+	// Scale the modulated LFO value so that it varies between 0 and 1
 	float scaled = (modulated + depth) / (2 * depth);
 
 	return scaled;
 }
 
-
 int main() {
-
+	// Define the path to the input WAV file
 	std::string input_file_path = "./Audio Files/audio.wav";
 
 	// Define the path to the output WAV file
@@ -75,41 +70,30 @@ int main() {
 
 	AudioFile<double> audioFile;
 	audioFile.load(input_file_path);
-	// Define the path to the input WAV file
+
+	// Initialize the circular buffer with the default delay value
+	DelayLine delay_line{};
 	delay_line.fill(0.0f);
-
-
-	// Initialize the time variable
-	int time = 0;
 
 	int sampling_rate = audioFile.getSampleRate();
 	// Compute the LFO frequency and delay time based on the sampling rate
 	float lfo_frequency = 2.0 / sampling_rate;
 
-
 	// Read the input samples from the WAV file
 	for (int i = 0; i < audioFile.getNumSamplesPerChannel(); i++)
 	{
-
 		// Generate the modulated LFO value
+		float lfo = ModulatedLfo(i, lfo_frequency, 0.1f);
 
-		float lfo = ModulatedLfo(time, lfo_frequency, 0.1f);
-		auto input = audioFile.samples[0][i];
 		// Apply the modulated delay to the input sample
-
 		float delay = min_delay + (max_delay - min_delay) * lfo;
-		float delayed = Delay(input, delay, sampling_rate);
+		float delayed = Delay(audioFile.samples[0][i], delay, sampling_rate);
 
 		// Mix the input sample and the delayed sample
-		audioFile.samples[0][i] = input + delayed;
-
-		// Increment the time variable
-		time++;
+		audioFile.samples[0][i] = audioFile.samples[0][i] + delayed;
 	}
 
-
 	audioFile.save(output_file_path);
-
 
 	return 0;
 }
